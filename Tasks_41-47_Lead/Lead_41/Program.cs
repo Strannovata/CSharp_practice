1f2e4050150e559358b66ffa9020bc51632b29a7
Anastacia <strannovata@gmail.com>
2022-01-27 00:08:52 +0300

    added tasks 41-42

---
+++ Tasks_41-47_Lead/Lead_41/Program.cs
@@ -0,0 +1,16 @@
+﻿// 41. Выяснить являются ли три числа сторонами треугольника
+
+bool IsTriangle(float a, float b, float c)
+{
+    if (((a + b) > c) && ((a + c) > b) && ((b + c) > a)) return true;
+    else return false;
+}
+
+float A = new Random().Next(0, 10);
+float B = new Random().Next(0, 10);
+float C = new Random().Next(0, 10);
+
+System.Console.WriteLine($"{A}, {B}, {C}");
+// System.Console.WriteLine(IsTriangle(A, B, C));
+if (IsTriangle(A, B, C) == true) System.Console.WriteLine("Числа являются сторонами треугольника");
+else System.Console.WriteLine("Числа не являются сторонами треугольника");
---
+++ Tasks_41-47_Lead/Lead_42/Program.cs
@@ -0,0 +1,32 @@
+﻿// 42. Определить сколько чисел больше 0 введено с клавиатуры
+
+int CountPositive(int N)
+{
+    int[] array = new int[N];
+    int i = 0;
+    int number = 1;
+    int count = 0;
+
+    for (i = 0; i < array.Length; i++)
+    {
+        System.Console.WriteLine($"Введите число {number}>>> ");
+        string S = System.Console.ReadLine();
+        array[i] = Convert.ToInt32(S);
+        number++;
+    }
+
+    for (i = 0; i < array.Length; i++)
+    {
+        System.Console.Write(array[i] + " ");
+    }
+    System.Console.WriteLine();
+    
+    for (i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0) count++;
+    }
+    return count;
+}
+
+int result = CountPositive(10);
+System.Console.WriteLine($"Это ваши числа, а количество положительных>>> {result}");
