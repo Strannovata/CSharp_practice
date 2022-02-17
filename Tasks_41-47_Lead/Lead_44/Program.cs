5ae03a1a96273b74d0f0f9e09e6a7fc7278e9a09
Anastacia <strannovata@gmail.com>
2022-02-17 15:55:17 +0300

    added task 44

---
+++ Tasks_41-47_Lead/Lead_44/Program.cs
@@ -0,0 +1,11 @@
+﻿// 44. Найти точку пересечения двух прямых заданных уравнением y=kx+b, b1 k1 и b2 k2 заданы
+
+void FindIntersection(int k1, int b1, int k2, int b2)
+{
+    float x = (k2 * b2 - k1 * b1) / (b1 - b2);
+    float y = k2 * x + b2;
+    System.Console.WriteLine($"({x}, {y})");
+}
+
+
+FindIntersection(1, 3, 2, 4);
