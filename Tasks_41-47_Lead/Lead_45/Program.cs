6ecc56f6584175fc6ce6d33fe610ca1e4aabcc9a
Anastacia <strannovata@gmail.com>
2022-02-22 14:18:24 +0300

    added tasks 45-46

---
+++ Tasks_41-47_Lead/Lead_45/Program.cs
@@ -0,0 +1,20 @@
+﻿// 45. Показать числа Фибоначчи
+
+int Fibonacci(int n)
+{
+    if (n == 0 || n == 1) return n;
+    else return Fibonacci(n - 1) + Fibonacci(n - 2);
+}
+
+
+void PrintFib(int N)
+{
+    for (int i = 0; i < N; i++)
+    {
+        System.Console.Write($"{Fibonacci(i)} ");
+    }
+    System.Console.WriteLine();
+}
+
+
+PrintFib(20);
---
+++ Tasks_41-47_Lead/Lead_46/Program.cs
@@ -0,0 +1,18 @@
+﻿// 46. Написать программу масштабирования фигуры (квадрат/прямоугольник от центра)
+
+void Scale(double x0, double y0, double lenX, double lenY, double scale)
+{
+    double x1 = x0 - lenX / 2;
+    double x2 = x0 + lenX / 2;
+    double y1 = y0 - lenY / 2;
+    double y2 = y0 + lenY / 2;
+    System.Console.WriteLine($"Координаты начальных точек: A({x1}, {y1}), B({x1}, {y2}), C({x2}, {y2}), D({x2}, {y1})");
+    
+    double nx1 = x0 - scale * lenX / 2;
+    double nx2 = x0 + scale * lenX / 2;
+    double ny1 = y0 - scale * lenY / 2;
+    double ny2 = y0 + scale * lenY / 2;
+    System.Console.WriteLine($"Координаты новых точек при масштабировании в {scale} раза: A({nx1}, {ny1}), B({nx1}, {ny2}), C({nx2}, {ny2}), D({nx2}, {ny1})");
+}
+
+Scale(2, 3, 2, 4, 3);
